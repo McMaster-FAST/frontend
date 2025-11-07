4c66a272c62193a15a31792229cadddbc97873e2
Peter <peterwardell86@gmail.com>
2025-11-07 16:28:47 -0500

    Add all components we will be using. Build custom button component.

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { LucideIcon, ArrowRight } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center px-6 py-3 gap-2 !box-border whitespace-nowrap rounded-md font-poppins text-md font-medium transition-all hover:cursor-pointer disabled:text-disabled-secondary disabled:pointer-events-none focus-visible:ring-ring/30 focus-visible:ring-primary/50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        primary:
+          "bg-primary text-primary-foreground hover:bg-primary-hover hover:text-foreground disabled:bg-disabled-primary ",
+        secondary:
+          // Use an inset box-shadow to simulate a 2px inner border so layout/size is unchanged
+          "bg-secondary text-secondary-foreground shadow-[inset_0_0_0_2px_var(--color-secondary-foreground)] hover:bg-secondary-hover hover:text-background disabled:bg-disabled-primary disabled:shadow-[inset_0_0_0_2px_var(--color-disabled-secondary)]",
+        tertiary:
+          "hover:bg-tertiary text-tertiary-foreground hover:text-tertiary-hover-foreground focus-visible:ring-0",
+      }
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  }
+);
+
+function Button({
+  className,
+  variant,
+  asChild = false,
+  leftIcon: LeftIcon,
+  rightIcon: RightIcon,
+  children,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+    leftIcon?: LucideIcon;
+    rightIcon?: LucideIcon;
+  }) {
+  const Comp = asChild ? Slot : "button";
+  const strokeWidth = 3;
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, className }))}
+      {...props}
+    >
+      {LeftIcon && <LeftIcon strokeWidth={strokeWidth} />}
+      {children}
+      {RightIcon && <RightIcon strokeWidth={strokeWidth} />}
+      {variant === "tertiary" && (
+        <ArrowRight
+          className={!props.disabled ? "text-tertiary-hover-foreground" : ""}
+          strokeWidth={strokeWidth}
+        />
+      )}
+    </Comp>
+  );
+}
+
+export { Button, buttonVariants };
